ff8a8fca37e37d4836738a8a45c6c279bef00eec
dennan141 <dennis.karlsson96@outlook.com>
2024-04-25 16:21:03 +0200

    Fixed add event

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,27 @@
-export default function Form(functionName) {
+"use client";
+import * as db from "@/Components/IndexedDb/Database.js";
+
+export default function Form() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newEventTitle = event.target.Title.value;
+    const newEventDescription = event.target.Description.value;
+    const newEventDate = event.target.Date.value;
+
+    const newEvent = {
+      title: newEventTitle,
+      description: newEventDescription,
+      date: newEventDate
+    };
+    //Add event to database
+    db.set(newEvent);
+    return<h1>hej</h1>
+  };
+
+  // * --------- RENDER PAGE ---------
   return (
-    <form className="form-control m-2">
+    <form onSubmit={handleSubmit} className="form-control m-2">
       <label className="label" htmlFor="Title">
         Titel
       </label>
@@ -15,7 +36,7 @@ export default function Form(functionName) {
         Beskrivning
       </label>
       <textarea
-         className="input input-lg bg-secondary shadow w-full max-w-xs"
+        className="input input-lg bg-secondary shadow w-full max-w-xs"
         type="text-area"
         name="Description"
       />
@@ -24,12 +45,11 @@ export default function Form(functionName) {
         Datum
       </label>
       <input
-         className="input  bg-secondary shadow w-full max-w-xs"
+        className="input  bg-secondary shadow w-full max-w-xs"
         type="datetime-local"
-        name=""
-        id=""
+        name="Date"
       />
-      <button className="btn max-w-xs mt-2" onClick={functionName}>Lägg till</button>
+      <button className="btn max-w-xs mt-2">Lägg till</button>
     </form>
   );
 }
